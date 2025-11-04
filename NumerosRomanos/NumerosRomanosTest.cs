39a1296348b1a4d6e2aeea82e26096110180406d
Ivan Rene Monroy Santos <ivan.monroy@sinco.com.co>
2025-11-04 18:59:04 -0500

    🟥Si_SeConvierteNumeroUno_Debe_RetornarI

---
+++ NumerosRomanos/NumerosRomanosTest.cs
@@ -0,0 +1,24 @@
+﻿using FluentAssertions;
+
+namespace NumerosRomanos;
+
+public class NumerosRomanosTest
+{
+    [Fact]
+    public void Si_SeConvierteNumeroUno_Debe_RetornarI()
+    {
+        var numerosRomanos = new ConvertidorNumerosRomanos();
+        
+        var conversion = numerosRomanos.Convertir(1);
+        
+        conversion.Should().Be("I"); 
+    }
+}
+
+public class ConvertidorNumerosRomanos
+{
+    public object Convertir(int i)
+    {
+        throw new NotImplementedException();
+    }
+}
---
+++ NumerosRomanos/TestList.md
@@ -0,0 +1,12 @@
+[X] Si se convierte 1 debe retornar I
+[] Si se convierte 2 debe retornar II
+[] Si se convierte 3 debe retornar III
+[] Si se convierte 4 debe retornar IV
+[] Si se convierte 5 debe retornar V
+[] Si se convierte 6 debe retornar VI
+[] Si se convierte 8 debe retornar VIII 
+
+
+
+
+
